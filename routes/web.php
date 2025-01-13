2a27203215060ac3e0b10b5d34815b521902f04a
codingghoul7 <the.raushankr@gmail.com>
2025-01-13 19:54:48 +0530

    Job as a class

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,11 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/jobs', function () {
-    return view('/jobs', [
-        "jobs" => [
+class Job
+{
+    public static function all(): array
+    {
+        return  [
             [
                 'id' => 1,
                 'title' => "Web Developer",
@@ -25,8 +23,18 @@ Route::get('/jobs', function () {
                 'title' => "IOS Developer",
                 "salary" => "30000$",
             ]
-        ]
-    ]);
+        ];
+    }
+}
+
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/jobs', function () {
+    return view('/jobs', ['jobs' => Job::all()]);
 });
 
 Route::get('/contact', function () {
@@ -66,29 +74,15 @@ Route::get('/lobs/{id}', function ($id) {
 
 
     return view('jobs', ['job' => $job]);
-});
+})->middleware('Cors');
 
+
+//For Grouped
 Route::middleware([\App\Http\Middleware\Cors::class])->group(function ($id) {
     Route::get('/jobs/{id}', function ($id) {
-        $jobs = [
-            [
-                'id' => 1,
-                'title' => "Web Developer",
-                "salary" => "3000$",
-            ],
-            [
-                'id' => 2,
-                'title' => "Android Developer",
-                "salary" => "8000$",
-            ],
-            [
-                'id' => 3,
-                'title' => "IOS Developer",
-                "salary" => "30000$",
-            ]
-        ];
+
         // Return the job data
-        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+        $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
 
 
         // dd($job); tocheck the function returns the found job and so we can pass it
